750e65eed528bde3885ae1fadbbd7a48a306e37a
Berihun Tsegaye <berihuntsegaye6@gmail.com>
2024-07-08 09:54:25 +0300

    fix: deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Myprojects from './components/myprojects/Myprojects'
-import Contacts from './components/contacts/Contacts'
-import Footer from './components/footer/Footer'
-import Skills from './components/experience/Skills'
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Myprojects from "./components/myprojects/Myprojects";
+import Contacts from "./components/contacts/Contacts";
+import Footer from "./components/footer/Footer";
+import Skills from "./components/experience/Skills";
 
 const App = () => {
   return (
-    
     <>
-        <Header/>
-        <Nav/>
-        <About/>
-        <Skills/>
-        <Experience/>
-        <Myprojects/>
-        <Contacts/>
-        <Footer/>
-
+      <Header />
+      <Nav />
+      <About />
+      <Skills />
+      <Experience />
+      <Myprojects />
+      <Contacts />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
